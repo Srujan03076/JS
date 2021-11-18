c71381578ec7cf16fabc75896a95c759c96bc742
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-19 01:14:49 +0530

    [Srujan]Added code for display Week days using JS

---
+++ Case Problem/WeekDays.js
@@ -0,0 +1,29 @@
+const prompt = require("prompt-sync") ();
+const number = parseInt(prompt("Enter a Number: "));
+
+switch (number) {
+    case 1:
+        console.log('SUNDAY')
+        break;
+    case 2:
+        console.log('MONDAY')
+        break;
+    case 3:
+        console.log('TUESDAY')
+        break;
+    case 4:
+        console.log('WEDNESDAY')
+        break;
+    case 5:
+        console.log('THIRSDAY')
+        break;
+    case 6:
+        console.log('FRIDAY')
+        break;
+    case 7:
+        console.log('SATURDAY')
+        break;
+    default:
+        console.log('Single Digit only!')
+        break;
+}
