5270f7101fb78111fbe1e9d532b6acbbc146ad39
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-18 23:15:00 +0530

    [Srujan]Added code for If&Else Problem

---
+++ If&Else ConditionProblem/Min&MaxBetween5Random.js
@@ -0,0 +1,9 @@
+let random1=Math.floor((Math.random()*900)+99);
+let random2=Math.floor((Math.random()*900)+99);
+let random3=Math.floor((Math.random()*900)+99);
+let random4=Math.floor((Math.random()*900)+99);
+let random5=Math.floor((Math.random()*900)+99);
+
+console.log("Maximum value is = "+Math.max(random1,random2,random3,random4,random5));
+
+console.log("Minimum value is = "+Math.min(random1,random2,random3,random4,random5));
