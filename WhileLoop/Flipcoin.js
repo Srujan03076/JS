032384d1fa62b8db3d6e98cea15cca5e58fa71a8
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-20 14:36:10 +0530

    [Srujan]Added code to added flip coin to get heads or tails 11 times using JS

---
+++ WhileLoop/Flipcoin.js
@@ -0,0 +1,24 @@
+let headCount=0;
+let tailCount=0;
+while(headCount<=10 || tailCount<=10)
+{
+    let flip=Math.floor(Math.random()*10)%2;
+    if(flip==0)
+    {
+        console.log("Head")
+        headCount++;
+    }
+    else
+    {
+        console.log("Tail")
+        tailCount++;
+    }
+}
+if(headCount==11)
+{
+    console.log("Head is the Winner");
+}
+else
+{
+    console.log("Tail is the Winner");
+}
